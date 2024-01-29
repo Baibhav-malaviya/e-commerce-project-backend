40d479ef221dbbb611ca5736f5a8ffceeb28453e
Baibhav Kumar <baibhav.kr73@gmail.com>
2024-01-29 14:42:56 +0530

    Changing the order model from one-to-many to one-to-one. It means previously there are multiple products in a single order but now there is only one product per order

--- Src/models/order.model.js
+++ Src/models/order.model.js
@@ -3,25 +3,21 @@ const { Schema, model } = require("mongoose");
 const orderSchema = new Schema(
     {
         user: {
-            type: mongoose.Schema.Types.ObjectId,
+            type: Schema.Types.ObjectId,
             ref: "User",
             required: true,
         },
-        products: [
-            {
-                product: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Product",
-                    required: true,
-                },
-                quantity: {
-                    type: Number,
-                    required: true,
-                    min: 1,
-                },
-            },
-        ],
-        totalPrice: {
+        product: {
+            type: Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,
+        },
+        price: {
             type: Number,
             required: true,
             min: 0,
@@ -48,3 +44,49 @@ const orderSchema = new Schema(
 const Order = model("Order", orderSchema);
 
 module.exports = Order;
+
+//? This is the orderSchema in which in a single order there are multiple products, then there are problems in tracking a particular products or in fetching the info of a particular product.
+// const orderSchema = new Schema(
+//     {
+//         user: {
+//             type: Schema.Types.ObjectId,
+//             ref: "User",
+//             required: true,
+//         },
+//         products: [
+//             {
+//                 product: {
+//                     type: Schema.Types.ObjectId,
+//                     ref: "Product",
+//                     required: true,
+//                 },
+//                 quantity: {
+//                     type: Number,
+//                     required: true,
+//                     min: 1,
+//                 },
+//             },
+//         ],
+//         totalPrice: {
+//             type: Number,
+//             required: true,
+//             min: 0,
+//         },
+//         shippingAddress: {
+//             type: String,
+//             required: true,
+//         },
+//         orderStatus: {
+//             type: String,
+//             enum: [
+//                 "pending",
+//                 "processing",
+//                 "shipped",
+//                 "delivered",
+//                 "cancelled",
+//             ],
+//             default: "pending",
+//         },
+//     },
+//     { timestamps: true }
+// );
